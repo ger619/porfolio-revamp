4a22fd56a6e3b2ba8baf48fd225f231a3c07a494
David Abol Ger <draker74@gmail.com>
2022-10-31 21:07:40 +0300

    Add details

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,10 +3,9 @@ import Loader from 'react-loaders';
 import MenuLogo from '../../assets/images/menu-logo.png';
 import HTML5 from '../../assets/images/HTML5_logo.png';
 import CSS from '../../assets/images/css_logo.png';
-
-// import ReactLogo from '../../assets/images/reactjs.png';
+import Coin from '../../assets/images/coin.png';
 import Webpack from '../../assets/images/jsweb.png';
-// import ReactRails from '../../assets/images/railsreact.png';
+import Reactjs from '../../assets/images/reactjs.png';
 //              <a target="_blank" rel="noreferrer" href="https://tomb-stone.herokuapp.com/users/sign_in">
 //  <a target="_blank" rel="noreferrer" href="https://deploy-preview-1--storied-cobbler-ece3f1.netlify.app/">
 // <a target="_blank" rel="noreferrer" href="https://lusindiso.github.io/Javascript-Capstone/">
@@ -21,7 +20,7 @@ const Gallery = () => (
         <div className="gallery-grid">
           <div className="card">
             <div>
-              <img className="card-image" src={MenuLogo} alt="logo" width="200px" />
+              <img className="card-image" src={MenuLogo} alt="logo" height="150px" />
             </div>
             <div className="card-text">
               <h2>A Hotel Menu</h2>
@@ -41,11 +40,12 @@ const Gallery = () => (
           </div>
           <div className="card">
             <div>
-              <img className="card-image" src={MenuLogo} alt="logo" width="200px" />
+              <img className="card-image" src={Coin} alt="logo" height="150px" />
             </div>
             <div className="card-text">
-              <h2>A Hotel Menu</h2>
-              <h3>A hotel menu built with Webpack/Vanillajs, HTML, CSS and we consume an API </h3>
+              <h2>Ger Crypto App</h2>
+              {/* eslint-disable-next-line max-len */}
+              <h3>App that consumes RapidApi, is built on React and Redux, and hosted on Netifly</h3>
               <a target="_blank" rel="noreferrer" href="https://lusindiso.github.io/Javascript-Capstone/">
                 <button type="button" className="flat-button">See Live</button>
               </a>
@@ -54,7 +54,7 @@ const Gallery = () => (
               </a>
             </div>
             <div className="card-stats">
-              <img src={Webpack} alt="webpack" height="72px" />
+              <img src={Reactjs} alt="React" height="72px" />
               <img src={HTML5} alt="html 5" height="72px" />
               <img src={CSS} alt="webpack" height="72px" />
             </div>
