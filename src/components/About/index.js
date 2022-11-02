600764fa4ebab2fd0d6bf4b17cd0ae1941843f9c
David Abol Ger <draker74@gmail.com>
2022-11-02 16:12:07 +0300

    add skills and languages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,9 @@ import Loader from 'react-loaders';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import RailsLogo from '../../assets/images/rails_logo.png';
+import Reactjs from '../../assets/images/reactjs.png';
+import Webpack from '../../assets/images/jsweb.png';
+import Ruby from '../../assets/images/ruby.png';
 
 const About = () => (
   <>
@@ -24,19 +27,64 @@ const About = () => (
 
       <div className="stage-cube-cont">
         <div>
-          <h1>Languages and Frameworks</h1>
-          <div style={{ width: 120, height: 120 }}>
+          <h1>Languages</h1>
+          <div className="lang" style={{ width: 120, height: 120 }}>
             <CircularProgressbarWithChildren value={50} style={{ color: 'yellow' }}>
-              {/* eslint-disable-next-line max-len */}
+              <img style={{ width: 62, marginTop: -5 }} src={Ruby} alt="Ruby" />
+              <br />
+              <div style={{ fontSize: 12, marginTop: -15 }}>
+                <strong>50%</strong>
+              </div>
+            </CircularProgressbarWithChildren>
+          </div>
+          <div className="lang" style={{ width: 120, height: 120 }}>
+            <CircularProgressbarWithChildren value={72} style={{ color: 'yellow' }}>
+              <img style={{ width: 62, marginTop: -5 }} src={Reactjs} alt="React" />
+              <br />
+              <div style={{ fontSize: 12, marginTop: -15 }}>
+                <strong>72%</strong>
+              </div>
+            </CircularProgressbarWithChildren>
+          </div>
+          <div className="lang" style={{ width: 120, height: 120 }}>
+            <CircularProgressbarWithChildren value={45} style={{ color: 'yellow' }}>
+              <img style={{ width: 62, marginTop: -5 }} src={Webpack} alt="React" />
+              <br />
+              <div style={{ fontSize: 12, marginTop: -20 }}>
+                <strong>45%</strong>
+              </div>
+            </CircularProgressbarWithChildren>
+          </div>
+        </div>
+        <div>
+          <h1>Frameworks</h1>
+          <div className="lang" style={{ width: 120, height: 120 }}>
+            <CircularProgressbarWithChildren value={50} style={{ color: 'yellow' }}>
               <img style={{ width: 72, marginTop: -5 }} src={RailsLogo} alt="React" />
               <br />
               <div style={{ fontSize: 12, marginTop: -20 }}>
                 <strong>50%</strong>
               </div>
             </CircularProgressbarWithChildren>
-            ;
           </div>
-
+          <div className="lang" style={{ width: 120, height: 120 }}>
+            <CircularProgressbarWithChildren value={72} style={{ color: 'yellow' }}>
+              <img style={{ width: 72, marginTop: -5 }} src={Reactjs} alt="React" />
+              <br />
+              <div style={{ fontSize: 12, marginTop: -20 }}>
+                <strong>72%</strong>
+              </div>
+            </CircularProgressbarWithChildren>
+          </div>
+          <div className="lang" style={{ width: 120, height: 120 }}>
+            <CircularProgressbarWithChildren value={45} style={{ color: 'yellow' }}>
+              <img style={{ width: 72, marginTop: -5 }} src={Webpack} alt="React" />
+              <br />
+              <div style={{ fontSize: 12, marginTop: -20 }}>
+                <strong>45%</strong>
+              </div>
+            </CircularProgressbarWithChildren>
+          </div>
         </div>
       </div>
     </div>
