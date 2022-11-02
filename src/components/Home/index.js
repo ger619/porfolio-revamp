2ae1b883ea216d72422890042add0725de83136f
David Abol Ger <draker74@gmail.com>
2022-11-02 18:32:02 +0300

    Add all data

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,18 +4,6 @@ import Loader from 'react-loaders';
 import LogoGer from '../../assets/images/layout-home.png';
 import Logo from './Logo';
 
-const onButtonClick = () => {
-  fetch('../../assets/images/CV.pdf').then((response) => {
-    response.blob().then((blob) => {
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'CV.pdf';
-      a.click();
-    });
-  });
-};
-
 const Home = () => (
   <>
     <div className="container home-page">
@@ -32,9 +20,7 @@ const Home = () => (
         <h2>FrontEnd Developer (React)/ BackEnd Developer (Ruby on Rails)</h2>
         <h2>Diploma in Computer Information Systems</h2>
         <Link to="/contact" className="flat-button">CONTACT ME</Link>
-        {/* eslint-disable-next-line max-len */}
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-        <a onClick={onButtonClick} className="flat-button">DOWNLOAD CV</a>
+        <Link to="/CV.pdf" className="flat-button" target="_blank">DOWNLOAD CV</Link>
       </div>
       <Logo />
       <Loader type="pacman" />
