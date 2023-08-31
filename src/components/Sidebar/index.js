8cc99b9dccbd15c1d275ac2402aa47f72fd5c843
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-08-31 12:39:42 +0300

    Nav fix

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,28 +1,54 @@
-import './index.scss';
-import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./index.scss";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faPhone, faAt } from "@fortawesome/free-solid-svg-icons";
+import kijani_logo from "../../assets/images/kijani_logo.png";
+import logo from "../../assets/images/logo-nobg.png";
 import {
-  faGlobe, faPhone, faAt,
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  faLinkedin, faGithub, faTwitter, faAngellist, faInstagramSquare,
-} from '@fortawesome/free-brands-svg-icons';
+  faLinkedin,
+  faGithub,
+  faTwitter,
+  faAngellist,
+  faInstagramSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import { AiFillHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => (
   <>
     <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={logo} alt="logo" className="image" />
+      </Link>
       <nav>
         <NavLink exact="true" to="/" activeClassName="active">
-          <FontAwesomeIcon icon={faPhone} color="#ffffff" />
+          {/* <FontAwesomeIcon icon={faPhone} color="#ffffff" /> */}
+          <AiFillHome color="#ffffff" />
         </NavLink>
-        <NavLink exact="true" to="/about" activeClassName="active" className="about">
-          <FontAwesomeIcon icon={faAt} color="#ffffff" />
+        <NavLink
+          exact="true"
+          to="/about"
+          activeClassName="active"
+          className="about"
+        >
+          <img src={kijani_logo} alt="logo" className="image" />
         </NavLink>
-        <NavLink exact="true" to="/gallery" activeClassName="active" className="gallery">
-          <FontAwesomeIcon icon={faGlobe} color="#ffffff" />
+        <NavLink
+          exact="true"
+          to="/gallery"
+          activeClassName="active"
+          className="gallery"
+        >
+          {/* <FontAwesomeIcon icon={faGlobe} color="#ffffff" /> */}
+          <img src="/revinyo.png" alt="logo" className="image" />
         </NavLink>
-        <NavLink exact="true" to="/contact" activeClassName="active" className="contact">
-          <FontAwesomeIcon icon={faInstagramSquare} color="#ffffff" />
+        <NavLink
+          exact="true"
+          to="/contact"
+          activeClassName="active"
+          className="contact"
+        >
+          <img src="/duara.png" alt="logo" className="image" />
         </NavLink>
       </nav>
       <ul>
@@ -47,7 +73,6 @@ const Sidebar = () => (
           </a>
         </li>
       </ul>
-
     </div>
     <div className="nav-bar-left" />
   </>
