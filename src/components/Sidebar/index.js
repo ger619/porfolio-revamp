4e4c2c84b87cbe44b49455cb81cf29a6fcda03a0
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-08-31 12:50:06 +0300

    Added icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,7 @@
 import "./index.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe, faPhone, faAt } from "@fortawesome/free-solid-svg-icons";
-import kijani_logo from "../../assets/images/kijani_logo.png";
-import logo from "../../assets/images/logo-nobg.png";
 import {
   faLinkedin,
   faGithub,
@@ -11,8 +9,9 @@ import {
   faAngellist,
   faInstagramSquare,
 } from "@fortawesome/free-brands-svg-icons";
-import { AiFillHome } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { AiFillHome, AiFillInstagram } from "react-icons/ai";
+import kijani from "../../assets/images/kijani_logo.png";
+import logo from "../../assets/images/logo-nobg.png";
 
 const Sidebar = () => (
   <>
@@ -23,7 +22,7 @@ const Sidebar = () => (
       <nav>
         <NavLink exact="true" to="/" activeClassName="active">
           {/* <FontAwesomeIcon icon={faPhone} color="#ffffff" /> */}
-          <AiFillHome color="#ffffff" />
+          <AiFillHome color="#9e1b1d" />
         </NavLink>
         <NavLink
           exact="true"
@@ -31,7 +30,7 @@ const Sidebar = () => (
           activeClassName="active"
           className="about"
         >
-          <img src={kijani_logo} alt="logo" className="image" />
+          <img src={kijani} alt="logo" className="image" />
         </NavLink>
         <NavLink
           exact="true"
@@ -40,7 +39,7 @@ const Sidebar = () => (
           className="gallery"
         >
           {/* <FontAwesomeIcon icon={faGlobe} color="#ffffff" /> */}
-          <img src="/revinyo.png" alt="logo" className="image" />
+          <img src="/revin.png" alt="logo" className="image" />
         </NavLink>
         <NavLink
           exact="true"
@@ -54,22 +53,7 @@ const Sidebar = () => (
       <ul>
         <li>
           <a target="_blank" rel="noreferrer" href="/">
-            <FontAwesomeIcon icon={faLinkedin} color="#ffffff" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="/">
-            <FontAwesomeIcon icon={faAngellist} color="#ffffff" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="/">
-            <FontAwesomeIcon icon={faGithub} color="#ffffff" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="/">
-            <FontAwesomeIcon icon={faTwitter} color="#ffffff" />
+            <AiFillInstagram color="#ffffff" />
           </a>
         </li>
       </ul>
